954ecd5e5628f5315fbe89ab8ca14b5e14dc032a
umitsuki <85103168+umitsuki-ovo@users.noreply.github.com>
2024-11-22 01:28:24 +0900

    Create play_ascii.py

---
+++ play_ascii.py
@@ -0,0 +1,29 @@
+import os
+import sys
+import time
+
+ascii_path = sys.argv[1]
+adjust = 1.14
+
+def load_ascii(ascii_path):
+    with open(ascii_path, 'r') as file:
+        ascii_data = file.read()
+        data = ascii_data.split('\n\n')
+    return data
+
+def play_animation(data, fps):
+    os.system('cls')
+    delay = 1.0 / (fps * adjust)
+    for ascii in data:
+        print("\033[H", end="")
+        print(ascii)
+        time.sleep(delay)
+
+def main():
+    os.system('cls')
+    print('loading...')
+    data = load_ascii(ascii_path)
+    fps = int(float(data.pop(0)))
+    play_animation(data, fps)
+
+main()
